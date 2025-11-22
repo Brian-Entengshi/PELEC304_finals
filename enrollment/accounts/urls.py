36011114a5a678a5c705688e6e9bd98ee4003bb3
Brian-Entengshi <brianencina524@gmail.com>
2025-11-22 21:41:14 +0800

    Delete enrollment directory

--- enrollment/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register_user, name='register'),
-    path('', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-]
--- enrollment/online_enrollment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Course, Tuition, Instructor, Semester, Student
-
-admin.site.register(Course)
-admin.site.register(Tuition)
-admin.site.register(Instructor)
-admin.site.register(Semester)
-admin.site.register(Student)
